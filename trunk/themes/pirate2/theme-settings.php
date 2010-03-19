f4aa1dd86eefea0af0e50199f1dd78f62f8b5965
Apophis <Apophis@f14a0bf8-7986-4ec7-8727-56bb208eb14b>
2010-03-19 09:43:00 +0000

    favicon.ico changed fix for theme-settings
    git-svn-id: http://projects.piratenpartei.ch:8080/drupalng@17 f14a0bf8-7986-4ec7-8727-56bb208eb14b

--- trunk/themes/pirate2/theme-settings.php
+++ trunk/themes/pirate2/theme-settings.php
@@ -24,15 +24,13 @@ function pirate2_settings($saved_settings) {
   /*
    * Create the form using Forms API: http://api.drupal.org/api/6
    */
-  $form = array();
-  /* -- Delete this line if you want to use this setting
+  $form = array();  
   $form['pirate2_example'] = array(
     '#type'          => 'checkbox',
     '#title'         => t('Use this sample setting'),
     '#default_value' => $settings['pirate2_example'],
     '#description'   => t("This option doesn't do anything; it's just an example."),
   );
-  // */
 
   // Add the base theme's settings.
   $form += zen_settings($saved_settings, $defaults);
