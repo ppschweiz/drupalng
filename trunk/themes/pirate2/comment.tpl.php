ded958d85624bd6a8cfc01fd2192e025784288c1
Apophis <Apophis@f14a0bf8-7986-4ec7-8727-56bb208eb14b>
2010-05-06 14:44:57 +0000

    git-svn-id: http://projects.piratenpartei.ch:8080/drupalng@91 f14a0bf8-7986-4ec7-8727-56bb208eb14b

---
+++ trunk/themes/pirate2/comment.tpl.php
@@ -0,0 +1,15 @@
+<ul class="comment">
+    <li class="cotermin">
+        <h4><?=$month?></h4>
+        <h5><?=$day?></h5>
+    </li>
+    <li class="cotext">
+        <a class="user"><?=$author?></a>
+        <h1><?= $title ?></h1>
+        <?= $content ?>
+        <div class="cofoleft">
+           <span class="date"><?= $date ?></span>
+        </div>
+    </li>
+    <li class="clear"/>
+</ul>
