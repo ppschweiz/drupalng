d69a95575b916641491bec8c74e2034b7e3a3fe7
danilo <danilo@f14a0bf8-7986-4ec7-8727-56bb208eb14b>
2010-05-06 13:53:19 +0000

    Bug #27 gefixt, Text im Footer auf Adresse geändert, Link auf Kontaktformular
    git-svn-id: http://projects.piratenpartei.ch:8080/drupalng@84 f14a0bf8-7986-4ec7-8727-56bb208eb14b

--- trunk/themes/pirate2/page.tpl.php
+++ trunk/themes/pirate2/page.tpl.php
@@ -157,7 +157,7 @@
 		<div class="wrapper">
 			<div id="fooleft">
 				<a class="imgcc" href="#"></a>
-				<p><b>Design: </b><a href="http://www.pirateparty.ch">Pirate Party Switzerland</a> <br />
+				<p><b>Design: </b><a href="/kontaktformular">Piratenpartei Schweiz, Postfach, 1337 Vallorbe, info@piratenpartei.ch</a> <br />
 					<a class="impressum" href="http://creativecommons.org/licenses/by/2.5/ch/deed.en_CA">CC-BY 2.5 Switerland</a>
 				</p>
 			</div>
