f1436b4e700270f8012ec64ab299f7f9d5b3e01c
Apophis <Apophis@f14a0bf8-7986-4ec7-8727-56bb208eb14b>
2010-05-17 15:43:59 +0000

    git-svn-id: http://projects.piratenpartei.ch:8080/drupalng@140 f14a0bf8-7986-4ec7-8727-56bb208eb14b

--- trunk/themes/pirate2/block.tpl.php
+++ trunk/themes/pirate2/block.tpl.php
@@ -1,10 +1,13 @@
 <?php
 // $Id: block.tpl.php,v 1.3 2007/08/07 08:39:36 goba Exp $
 ?>
-	<?php  print $id.":".$block->module."<=>".$block->region; print_r(views_get_view("Politblog")); ?>
 	<?php if ($block->module == 'pirate_slider' || $block->module == 'pirate_actionbutton') { ?>
 		<?php print $block->content; ?>
 	<?php } else if ($is_front) { ?>
+		<?php  
+			print_r($block);
+		?>
+
 		<div class="kopf block block-<?php print $block->module; ?>" id="block-<?php print $block->module; ?>-<?php print $block->delta; ?>">
 			<a class="goto" href=""></a>
 			<a class="rss" href=""></a>
