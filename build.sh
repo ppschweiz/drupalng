1e0f74f318d4ed149f0aa1db86c6bea04b86a6b4
Corvus <christian.haeusler@piratenpartei.ch>
2012-08-26 11:06:19 +0200

    Group sites in folder

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf public
 drush make pps.make public --translations=de,fr,it
 
 rm -r public/sites/default
-ln -s ../../default public/sites/default
+ln -s ../../sites/default public/sites/default
 
 ln -s ../../../../themes/pps_default public/sites/all/themes/pps_default
 ln -s ../../../../modules/ public/sites/all/modules/custom
