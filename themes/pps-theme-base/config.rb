b62a475ee58fd04deb509ee0a4a88dbb6d1666ad
Corvus <christian.haeusler@piratenpartei.ch>
2012-12-13 19:10:52 +0100

    Base theme
    Respawned the base theme from the current at_subtheme.

---
+++ themes/pps-theme-base/config.rb
@@ -0,0 +1,21 @@
+# Location of the theme's resources.
+# Compass will overwrite the CSS files in the css_dir when set to watch.
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "css/images"
+
+#output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :production) ? :compressed : :expanded
+
+line_comments = (environment == :development)
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from server root.
+relative_assets = true
+
+# Pass options to sass.
+# - For production, we force the CSS to be regenerated even though the source
+#   scss may not have changed, since we want the CSS to be compressed and have
+#   the debug info removed.
+sass_options = {:always_update => true} if (environment == :production)
---
+++ themes/pps-theme-base/template.php
@@ -0,0 +1,118 @@
+<?php
+
+include_once(drupal_get_path('theme', 'pps_theme_base') . '/includes/theme.inc');
+
+/**
+ * @file
+ * Process theme data.
+ *
+ * Use this file to run your theme specific implimentations of theme functions,
+ * such preprocess, process, alters, and theme function overrides.
+ *
+ * Preprocess and process functions are used to modify or create variables for
+ * templates and theme functions. They are a common theming tool in Drupal, often
+ * used as an alternative to directly editing or adding code to templates. Its
+ * worth spending some time to learn more about these functions - they are a
+ * powerful way to easily modify the output of any template variable.
+ * 
+ * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
+ * 1. Rename each function and instance of "pps_theme_base" to match
+ *    your subthemes name, e.g. if your theme name is "footheme" then the function
+ *    name will be "footheme_preprocess_hook". Tip - you can search/replace
+ *    on "pps_theme_base".
+ * 2. Uncomment the required function to use.
+ */
+
+
+/**
+ * Preprocess variables for the html template.
+ */
+/* -- Delete this line to enable.
+function pps_theme_base_preprocess_html(&$vars) {
+  global $theme_key;
+
+  // Two examples of adding custom classes to the body.
+  
+  // Add a body class for the active theme name.
+  // $vars['classes_array'][] = drupal_html_class($theme_key);
+
+  // Browser/platform sniff - adds body classes such as ipad, webkit, chrome etc.
+  // $vars['classes_array'][] = css_browser_selector();
+
+}
+// */
+
+
+/**
+ * Process variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function pps_theme_base_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function pps_theme_base_preprocess_page(&$vars) {
+}
+function pps_theme_base_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+function pps_theme_base_preprocess_node(&$vars) {
+  // Build the date_icon variable used in node templates
+  if (variable_get('node_submitted_' . $vars['node']->type, TRUE)) {
+    $date_template =<<<EOF
+<time datetime="!datetime" pubdate="pubdate" title='!title' class="icon">
+  <span class='month'>!month</span>
+  <span class='day'>!day</span>
+  <span class='year'>!year</span>
+</time>
+EOF;
+
+    $date_args = array(
+      '!datetime' => $vars['datetime'],
+      '!title' => $vars['date'],
+      '!year' => format_date($vars['created'], 'custom', 'Y'),
+      '!month' => format_date($vars['created'], 'custom', 'M'),
+      '!day' => format_date($vars['created'], 'custom', 'd'),
+    );
+    $vars['date_icon'] = format_string($date_template, $date_args);
+  }
+  else {
+    $vars['date_icon'] = '';
+  }
+}
+/* -- Delete this line if you want to use these functions
+function pps_theme_base_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function pps_theme_base_preprocess_comment(&$vars) {
+}
+function pps_theme_base_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function pps_theme_base_preprocess_block(&$vars) {
+}
+function pps_theme_base_process_block(&$vars) {
+}
+// */
